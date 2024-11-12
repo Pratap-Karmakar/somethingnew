69dffde4ce799952c23bac739931a2144ccaacb0
Pratap Karmakar <pratap2501.pk@gmail.com>
2024-11-12 20:21:09 +0530

    some changes

--- src/sections/OurProjects.tsx
+++ src/sections/OurProjects.tsx
@@ -56,16 +56,16 @@ const OurProjects: React.FC = () => {
   return (
     <div className="relative w-full py-20 mt-20">
       {/* Two Yellow Circles at Top Right Corner */}
-      <div className="absolute top-0 right-0 w-80 h-80 bg-yellow-500 rounded-full opacity-50 -translate-x-1/4 -translate-y-1/4"></div>
-      <div className="absolute top-10 right-20 w-64 h-64 bg-yellow-500 rounded-full opacity-50 -translate-x-1/4 -translate-y-1/4"></div>
+      {/* <div className="absolute top-0 right-0 w-80 h-80 bg-yellow-500 rounded-full opacity-50 -translate-x-1/4 -translate-y-1/4"></div>
+      <div className="absolute top-10 right-20 w-64 h-64 bg-yellow-500 rounded-full opacity-50 -translate-x-1/4 -translate-y-1/4"></div> */}
 
-      <div className="w-full px-20 border-b border-t border-zinc-700 pb-20 relative z-10">
+      <div className="w-full px-2 md:px-20 lg:px-20 xl:px-20 border-b border-t border-zinc-700 pb-20 relative z-10">
         <h1 className="md:text-7xl lg:text-7xl xl:text-8xl text-3xl font-bold tracking-tight text-black pt-10 pb-20">
           Our Projects.
         </h1>
 
         {/* Adjust layout for different screen sizes */}
-        <div className="cards w-full grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="cards w-full grid grid-cols-2 md:grid-cols-2 md:gap-10 lg:gap-10 xl:gap-10 gap-3">
           {projectData.map((project) => (
             <div
               key={project.name}
@@ -106,7 +106,7 @@ const OurProjects: React.FC = () => {
 
               {/* Card Image with Title overlay for mobile screens */}
               <div className="card w-full h-full rounded-xl overflow-hidden hover:cursor-pointer hover:scale-105 transition-all duration-500 relative">
-                <img src={project.imgSrc} alt={`${project.name} Project`} />
+                <img src={project.imgSrc} alt={`${project.name} Project`} className="hover:scale-110 transition-all duration-500" />
                 {/* Title overlay for mobile devices */}
                 <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center md:hidden">
                   <h1 className="text-4xl font-bold text-yellow-500">
